c0a077cde51553afd58465fda6b7dd015a41a384
Sirish Bathina <sirish.bathina@veeam.com>
2024-03-12 12:43:20 -1000

    update pbkdf2 to the openssl version

--- pbkdf2/pbkdf2.go
+++ pbkdf2/pbkdf2.go
@@ -15,12 +15,16 @@ HMAC-SHA1, the drafted v2.1 specification allows use of all five FIPS Approved
 Hash Functions SHA-1, SHA-224, SHA-256, SHA-384 and SHA-512 for HMAC. To
 choose, you can pass the `New` functions from the different SHA packages to
 pbkdf2.Key.
+
+This package overrides the original pbkdf2 provided by golang.org/x/crypto with
+the FIPS approved pbkdf2 provided by github.com/golang-fips/opensssl/v2
 */
 package pbkdf2 // import "golang.org/x/crypto/pbkdf2"
 
 import (
-	"crypto/hmac"
 	"hash"
+
+	opensslpbkdf "github.com/golang-fips/openssl/v2"
 )
 
 // Key derives a key from the password, salt and iteration count, returning a
@@ -40,38 +44,9 @@ import (
 // Using a higher iteration count will increase the cost of an exhaustive
 // search but will also make derivation proportionally slower.
 func Key(password, salt []byte, iter, keyLen int, h func() hash.Hash) []byte {
-	prf := hmac.New(h, password)
-	hashLen := prf.Size()
-	numBlocks := (keyLen + hashLen - 1) / hashLen
-
-	var buf [4]byte
-	dk := make([]byte, 0, numBlocks*hashLen)
-	U := make([]byte, hashLen)
-	for block := 1; block <= numBlocks; block++ {
-		// N.B.: || means concatenation, ^ means XOR
-		// for each block T_i = U_1 ^ U_2 ^ ... ^ U_iter
-		// U_1 = PRF(password, salt || uint(i))
-		prf.Reset()
-		prf.Write(salt)
-		buf[0] = byte(block >> 24)
-		buf[1] = byte(block >> 16)
-		buf[2] = byte(block >> 8)
-		buf[3] = byte(block)
-		prf.Write(buf[:4])
-		dk = prf.Sum(dk)
-		T := dk[len(dk)-hashLen:]
-		copy(U, T)
-
-		// U_n = PRF(password, U_(n-1))
-		for n := 2; n <= iter; n++ {
-			prf.Reset()
-			prf.Write(U)
-			U = U[:0]
-			U = prf.Sum(U)
-			for x := range U {
-				T[x] ^= U[x]
-			}
-		}
+	key, err := opensslpbkdf.PBKDF2(password, salt, iter, keyLen, h)
+	if err != nil {
+		panic(err)
 	}
-	return dk[:keyLen]
+	return key
 }
