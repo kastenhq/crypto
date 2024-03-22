93ad845f478233f5b75201ebb8ee1b3bf68397f0
Sirish Bathina <sirish.bathina@veeam.com>
2024-03-22 12:59:13 -1000

    hkdf2 switch between crypto/openssl

---
+++ hkdf/hkdf_fips.go
@@ -0,0 +1,16 @@
+//go:build fipsonly
+
+package hkdf
+
+import (
+	"hash"
+	"io"
+
+	_ "unsafe"
+)
+
+//go:linkname hkdfExtract vendor/github.com/golang-fips/openssl/v2.ExtractHKDF
+func hkdfExtract(h func() hash.Hash, secret, salt []byte) ([]byte, error)
+
+//go:linkname hkdfExpand vendor/github.com/golang-fips/openssl/v2.ExpandHKDF
+func hkdfExpand(h func() hash.Hash, pseudorandomKey, info []byte) (io.Reader, error)
