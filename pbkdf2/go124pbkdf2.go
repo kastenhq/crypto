aad2218d4c3926226ca09088c941c2b07c561e84
Eliott Gilson <eliott.gilson@veeam.com>
2025-06-25 22:17:35 -0700

    Added Go 1.24 build constraint to loop back to new std crypto

---
+++ pbkdf2/go124pbkdf2.go
@@ -0,0 +1,11 @@
+//go:build go1.24
+
+package pbkdf2
+
+import (
+	"crypto/pbkdf2"
+)
+
+func pbkdf2_key_derivation(password, salt []byte, iter, keyLen int, h func() hash.Hash) ([]byte, error) {
+	return pbkdf2.Key(h, string(password), salt, iter, keyLen)
+}
