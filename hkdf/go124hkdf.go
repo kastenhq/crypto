ccbf63cdbc1bdbe397e14f0ceec90d83ab4ddcb5
Eliott Gilson <eliott.gilson@veeam.com>
2025-06-25 23:19:38 -0700

    Fixed wrong name for calling hash function

--- hkdf/go124hkdf.go
+++ hkdf/go124hkdf.go
@@ -16,7 +16,7 @@ func hkdfExtract(h func() hash.Hash, secret, salt []byte) ([]byte, error) {
 func hkdfExpand(h func() hash.Hash, pseudorandomKey, info []byte) (io.Reader, error) {
 	// Since the golang.org/x/crypto signature doesn't use a key length
 	// we use the size of the hash
-	hashSize := hash().Size()
+	hashSize := h().Size()
 	b, err := hkdf.Expand(h, pseudorandomKey, info, hashSize)
 	return bytes.NewReader(b), err
 }
