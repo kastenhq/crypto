796b8a43ab7b91ac46b8489e121f69be879c4eff
Eliott Gilson <eliott.gilson@veeam.com>
2025-06-25 22:59:40 -0700

    Added Go 1.24 build constraint to loop back to new std crypto

---
+++ hkdf/go124hkdf.go
@@ -0,0 +1,20 @@
+//go:build go1.24
+
+package hkdf
+
+import (
+	"bytes"
+	"crypto/hkdf"
+)
+
+func hkdfExtract(h func() hash.Hash, secret, salt []byte) ([]byte, error) {
+	return hkdf.Extract(h, secret, salt)
+}
+
+func hkdfExpand(h func() hash.Hash, pseudorandomKey, info []byte) (io.Reader, error) {
+	// Since the golang.org/x/crypto signature doesn't use a key length
+	// we use the size of the hash
+	hashSize := hash().Size()
+	b, err := hkdf.Expand(h, pseudorandomKey, info, hashSize)
+	return bytes.NewReader(b), err
+}
